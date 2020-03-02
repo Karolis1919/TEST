7b8071ade29bf2445187664f41c9a11cf550482f
Arnoldas <support@coincrate.net>
2020-03-02 11:20:23 +0200

    perdaryt

---
+++ resources/views/admininfo/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('admininfo/main')
+
+@section('content')
+    @include('admininfo/_partials/blocks')
+    @include('admininfo/_partials/discussion')
+    @include('admininfo/_partials/mainnavbar')
+    @include('admininfo/_partials/quickpost')
+    @include('admininfo/_partials/referals')
+    @include('admininfo/_partials/sidebar')
+    @include('admininfo/_partials/stats')
+@stop
